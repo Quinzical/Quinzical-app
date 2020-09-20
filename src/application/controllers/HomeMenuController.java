4817abb2c43c9dcc66590d15ef7de59408df00bd
ZHENK <chengzhenyang@gmail.com>
2020-09-21 04:51:23 +1200

    add skeleton

---
+++ src/application/controllers/HomeMenuController.java
@@ -0,0 +1,28 @@
+package application.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HomeMenuController {
+
+    @FXML
+    void handleExitButton(ActionEvent event) {
+        // TODO 
+    }
+
+    @FXML
+    void handlePlayButton(ActionEvent event) {
+        // TODO 
+    }
+
+    @FXML
+    void handlePracticeButton(ActionEvent event) {
+        // TODO 
+    }
+
+    @FXML
+    void handleSettingsButton(ActionEvent event) {
+        // TODO 
+    }
+
+}
--- src/application/controllers/PracticeMenuController.java
+++ src/application/controllers/PracticeMenuController.java
@@ -1,5 +1,22 @@
 package application.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.TilePane;
+
 public class PracticeMenuController {
-    
+
+    @FXML
+    private TilePane _categoriesPane;
+
+    @FXML
+    void handleBackButton(ActionEvent event) {
+        // TODO
+    }
+
+    @FXML
+    void handleSettingsButton(ActionEvent event) {
+        // TODO
+    }
+
 }
