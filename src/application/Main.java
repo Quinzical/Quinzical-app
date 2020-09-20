9cf4a21fdd0a4a22dc7cb0976b84e976dcfb8769
ZHENK <chengzhenyang@gmail.com>
2020-09-21 05:25:01 +1200

    add test Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,41 @@
 package application;
 
-public class Main {
-    
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+public class Main extends Application {
+    private Scene _menu;
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+
+        // Initiate the first Scene - MainView
+        Parent root = FXMLLoader.load(getClass().getResource("/application/resources/HomeMenu.fxml"));
+        _menu = new Scene(root, 1280, 800);
+        _menu.getStylesheets().add("/application/resources/application.css");
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+            @Override
+            public void handle(final WindowEvent t) {
+                // Save game?
+            }
+        });
+        primaryStage.setTitle("Quinzical");
+        primaryStage.getIcons().add(new Image("/application/resources/images/darklogo.png"));
+        primaryStage.setScene(_menu);
+        primaryStage.setMinWidth(800);
+        primaryStage.setMinHeight(600);
+        primaryStage.show();
+    }
+
+    public static void main(final String[] args) {
+        launch();
+    }
 }
