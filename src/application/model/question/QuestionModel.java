3f47d3502f7f27e0b96275887a8c89760bf2ce10
mped822 <mped822@aucklanduni.ac.nz>
2020-09-22 10:34:56 +1200

    Added question model class and package

---
+++ src/application/model/question/QuestionModel.java
@@ -0,0 +1,61 @@
+package application.model.question;
+
+/**
+ * This class is used to delegate tasks to different classes who carry out tasks for the function of the practice module based on what the user wants to do.
+ *  
+ * @author Maggie Pedersen
+ * @author Cheng-Zhen Yang
+ *
+ */
+public class QuestionModel {
+
+	private static QuestionModel _instance;
+	
+	private boolean _practice;
+	private String _category;
+	private String _questionValue;
+	
+	private QuestionModel() {
+	}
+	
+	/**
+	 * Used to return the single instance of this class.
+	 * 
+	 * @return QuestionModel
+	 */
+	public static QuestionModel getInstance() {
+		if (_instance == null) {
+			_instance = new QuestionModel();
+		}
+		return _instance;
+	}
+	
+	/**
+	 * Used to indicate to the question model whether the question is being asked in the game or practice module.
+	 * 
+	 * @param practiceOrNot value should be true if practice module, false if games module
+	 */
+	public void setPractice(boolean practiceOrNot) {
+		_practice = practiceOrNot;
+	}
+	
+	/**
+	 * Used to set the category for both the games and practice module
+	 * 
+	 * @param category
+	 */
+	public void setCategory(String category) {
+		_category = category;
+	}
+	
+	/**
+	 * Used to set the current question value for use in the games module
+	 * 
+	 * @param value
+	 */
+	public void setQuestionValue(String value) {
+		_questionValue = value;
+	}
+	
+	
+}
