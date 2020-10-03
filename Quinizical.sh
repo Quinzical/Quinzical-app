84771b774f3fa67e3505859f2a3466e0a9cff9a3
ZHENK <chengzhenyang@gmail.com>
2020-10-04 02:08:24 +1300

    Compile to runnable jar v1

--- Quinizical.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java --module-path /home/se2062020/javafx-sdk-11.0.2/lib --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml -jar Quinizical.jar
---
+++ Quinzical.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java --module-path /home/se2062020/javafx-sdk-11.0.2/lib --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml -jar Quinzical.jar
