455e22ff783b1379330ece8fdbe07de20e614821
scorpionknifes <chengzhenyang@gmail.com>
2020-10-31 03:44:31 +1300

    Compile to runnable Jar

--- Quinzical.sh
+++ Quinzical.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java --module-path /usr/share/java/lib --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml -jar Quinzical.jar
+java --module-path /usr/share/java/lib --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml,javafx.web,javafx.graphics -jar Quinzical.jar
